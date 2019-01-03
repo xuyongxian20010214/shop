2d8c41ec9a605d4e49513b0cc80c3556c124f6a5
xuyongxian20010214 <3262999820@qq.com>
2019-01-03 15:55:17 +0800

    first commit

---
+++ app/Http/Controllers/User/UserController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Controllers\User;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+use App\Model\UserModel;
+
+class UserController extends Controller
+{
+    //
+
+	public function user($uid)
+	{
+		echo $uid;
+	}
+
+	public function add()
+	{
+		$data = [
+			'name'      => str_random(5),
+			'age'       => mt_rand(20,99),
+			'email'     => str_random(6) . '@gmail.com',
+			'reg_time'  => time()
+		];
+
+		$id = UserModel::insertGetId($data);
+		var_dump($id);
+	}
+}
---
+++ routes/web.php
@@ -0,0 +1,49 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+
+Route::get('/', function () {
+	//echo date("Y-m-d H:i:s");
+    return view('welcome');
+});
+
+Route::get('/adduser','User\UserController@add');
+
+//路由跳转
+Route::redirect('/hello1','/world1',301);
+Route::get('/world1','Test\TestController@world1');
+
+Route::get('hello2','Test\TestController@hello2');
+Route::get('world2','Test\TestController@world2');
+
+
+//路由参数
+Route::get('/user/{uid}','User\UserController@user');
+Route::get('/month/{m}/date/{d}','Test\TestController@md');
+Route::get('/name/{str?}','Test\TestController@showName');
+
+
+
+// View视图路由
+Route::view('/mvc','mvc');
+Route::view('/error','error',['code'=>403]);
+
+
+// Query Builder
+Route::get('/query/get','Test\TestController@query1');
+Route::get('/query/where','Test\TestController@query2');
+
+
+//用户注册
+Route::get('/userreg','User\UserController@reg');
+Route::post('/userreg','User\UserController@doReg');
